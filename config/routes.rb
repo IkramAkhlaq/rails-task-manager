25d9632180dd52f08744143a77623fca72a2f502
Ikram Akhlaq <ikram_akhlaq@hotmail.co.uk>
2020-03-12 11:00:41 +0000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # NEED TO ADD THE PREFIXES
+
+  # 1.Read Many
+  get 'tasks', to: 'tasks#index'
+  # 3. CreateNew, To display the form for the new task to be created
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # 4.PostNew, from the form what we have
+  post 'tasks', to: 'tasks#create'
+  # 2.Read Many
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # 5.UpdateDisplay, from the form what we have
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # 6.Then actually update from the database
+  patch 'tasks/:id', to: 'tasks#update'
+  # 7.destroy from a selected ID
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.destroy_all
+puts "destroyed all of them"
+
+Task.create(
+  title: 'Dishes',
+  details: 'clean up the dirty dishes',
+  completed: false
+)
+Task.create(
+  title: 'Floor',
+  details: 'clean up the dirty floor',
+  completed: true
+)
+Task.create(
+  title: 'Groceries',
+  details: 'buy some groceries',
+  completed: false
+)
+
+puts "created a few"
